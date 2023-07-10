bf2a406176f4bdac6d486d507d64d08530659b15
ronnygang <ronny.etk.77@gmail.com>
2023-07-10 10:04:25 -0500

    Adding spark tasks to dag

--- cloud_composer/datapipeline_gcp.py
+++ cloud_composer/datapipeline_gcp.py
@@ -16,6 +16,15 @@ from datetime import timedelta, datetime
 import logging
 import requests
 import random
+import uuid
+
+FECHA_FORMATO = datetime.now().strftime("%Y%m%d")
+UUID_4DIG = str(uuid.uuid4().hex)[:4]
+PROJECT_ID = 'ronny-dev-airflow'
+REGION = 'us-central1'
+CLUSTER_NAME = f'ephemeral-cluster-ronny-{FECHA_FORMATO}-{UUID_4DIG}'
+PYSPARK_URI = 'gs://dev-ronny-datalake-raw/scripts/transformaciones_pyspark.py'
+JAR_URI = "gs://dev-ronny-datalake-raw/scripts/spark-bigquery-with-dependencies_2.12-0.30.0.jar"
 
 DEFAULT_ARGS = {
     'owner': 'Ronny',
@@ -38,6 +47,27 @@ PATHS = {
     'campaigns_txt': 'gs://dev-ronny-datalake-raw/ingested/txt/campaigns_*.txt',
     'transactions_txt': 'gs://dev-ronny-datalake-raw/ingested/txt/transactions_*.txt',
     'loaded' : 'gs://dev-ronny-datalake-raw/loaded'
+}
+
+CLUSTER_CONFIG = {
+    "master_config": {
+        "num_instances": 1,
+        "machine_type_uri": "n1-standard-4",
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 128},
+    },
+    "worker_config": {
+        "num_instances": 3,
+        "machine_type_uri": "n1-standard-4",
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 128},
+    }
+}
+
+PYSPARK_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {"main_python_file_uri": PYSPARK_URI,
+    "jar_file_uris":[JAR_URI]
+    }
 }
 
 def make_curl_request(url):
@@ -87,8 +117,7 @@ with DAG(
         
 
 
-        with TaskGroup('load_to_bq_raw') as load_to_bq_raw:
-
+        with TaskGroup('load_raw_with_bq') as load_raw_with_bq:
             campaigns_ingested_sensor = GCSObjectsWithPrefixExistenceSensor(
                 task_id='campaigns_ingested_sensor',
                 bucket='dev-ronny-datalake-raw',
@@ -160,20 +189,81 @@ with DAG(
             
             starting_loading_to_raw >> [load_campaign, load_transaction] >> smart_cleaner
         
-        ingest_from_api_local >> load_to_bq_raw
+        ingest_from_api_local >> load_raw_with_bq
 
 
     with TaskGroup('api_global') as api_global:
-        ingest_campaigns_txt = PythonOperator(
-            task_id = 'ingest_campaigns_txt',
-            python_callable = make_curl_request,
-            op_args = [TASK_ARGUMENTS['url_campaigns_txt']]
-        )
-        ingest_transactions_txt = PythonOperator(
-            task_id = 'ingest_transactions_txt',
-            python_callable = make_curl_request,
-            op_args = [TASK_ARGUMENTS['url_transactions_txt']]
-        )
+        with TaskGroup('ingest_from_api_global') as ingest_from_api_global:
+            ingest_campaigns_txt = PythonOperator(
+                task_id = 'ingest_campaigns_txt',
+                python_callable = make_curl_request,
+                op_args = [TASK_ARGUMENTS['url_campaigns_txt']]
+            )
+
+            ingest_transactions_txt = PythonOperator(
+                task_id = 'ingest_transactions_txt',
+                python_callable = make_curl_request,
+                op_args = [TASK_ARGUMENTS['url_transactions_txt']]
+            )
+
+            [ingest_campaigns_txt , ingest_transactions_txt]
+        
+        with TaskGroup('load_raw_with_spark') as load_raw_with_spark:
+
+            campaigns_ingested_sensor = GCSObjectsWithPrefixExistenceSensor(
+                    task_id='campaigns_ingested_sensor',
+                    bucket='dev-ronny-datalake-raw',
+                    prefix='ingested/txt/campaigns_{{ ds_nodash }}_',
+                    google_cloud_conn_id='google_cloud_default',
+                    timeout = 15
+                )    
+
+            transactions_ingested_sensor = GCSObjectsWithPrefixExistenceSensor(
+                task_id='transactions_ingested_sensor',
+                bucket='dev-ronny-datalake-raw',
+                prefix='ingested/txt/transactions_{{ ds_nodash }}_',
+                google_cloud_conn_id='google_cloud_default',
+                timeout = 15
+            )
+
+            create_cluster = DataprocCreateClusterOperator(
+                task_id="create_cluster",
+                project_id=PROJECT_ID,
+                cluster_config=CLUSTER_CONFIG,
+                region=REGION,
+                cluster_name=CLUSTER_NAME,
+            )
+
+            pyspark_task = DataprocSubmitJobOperator(
+                task_id="data_pipeline_execution",
+                job=PYSPARK_JOB,
+                region=REGION,
+                project_id=PROJECT_ID
+            )
+
+            delete_cluster = DataprocDeleteClusterOperator(
+                task_id="delete_cluster",
+                project_id=PROJECT_ID,
+                cluster_name=CLUSTER_NAME,
+                region=REGION
+            )
+
+            smart_cleaner = BashOperator(
+                task_id = 'smart_cleaner',
+                bash_command = """
+                    gsutil mv {{ params.path_campaigns }} {{ params.target }} && 
+                    gsutil mv {{ params.path_transactions }} {{ params.target }}         
+                """,        
+                params = {
+                    'path_campaigns': PATHS['campaigns_txt'],
+                    'path_transactions': PATHS['transactions_txt'],
+                    'target': PATHS['loaded']
+                }
+            )
+
+            [campaigns_ingested_sensor, transactions_ingested_sensor] >> create_cluster >> pyspark_task
+
+            pyspark_task >> delete_cluster >> smart_cleaner
 
     start_datapipeline >> [api_local, api_global]
 
